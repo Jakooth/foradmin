a4ba159241f37ae67d174e18c9615cd0328e6870
Jakooth <ivan.k.vladimirov@gmail.com>
2017-01-29 21:55:26 +0200

    Add login action in Forplay
    Open the login dialog from the user profile menu.

--- controllers/login.js
+++ controllers/login.js
@@ -18,95 +18,95 @@ function LoginManager() {
 		"noConnectionError":             "Няма връзка към интернет.",
 		
 		"signin": {
-		"title":                            "Вход",
-		"action":                           "Влез",
-		"actionDomain":                     "Влез към адрес {domain}",
-		"all":                              "Не е твоят профил?",
-		"strategyEmailEmpty":               "Емайлът е празен.",
-		"strategyEmailInvalid":             "Емайлът е невалиден.",
-		"strategyDomainInvalid":            "Адресът {domain} не е настроен.",
-		"signinText":                       "Влез",
-		"signupText":                       "Регистрирай се",
-		"forgotText":                       "Пак са напи и не си помниш паролата?",
-		"cancelAction":                     "Отмени",
-		"footerText":                       "",
-		"emailPlaceholder":                 "Емайл",
-		"usernamePlaceholder":              "Форплеец",
-		"passwordPlaceholder":              "Парола",
-		"separatorText":                    "или",
-		"serverErrorText":                  "Имаше проблем при пенетрирането на входа.",
-		"returnUserLabel":                  "Последния път влиза като...",
-		"domainUserLabel":                  "Вие сте свързани от корпоративнат ви мрежа...",
-		"wrongEmailPasswordErrorText":      "Навлиден емайл или парола.",
-		"passwordChangeRequiredErrorText":  "Трябва да си смените паролата.",
-		"unauthorizedErrorText":            "Нямата достъп.",
-		"userBlockedErrorText":             "Профилът ви е блокриан",
-		"or":                               "... или влезта като",
-		"loadingMessage":                   "Влизане като {connection}...",
-		"popupCredentials":                 "Въведте кодовете си за достъп",
-		"userClosedPopup":                  "Прозорецът се затвори. Опитай пак.",
-		"userConsentFailed":                "Нямате позволение. Опитай пак."
+			"title":                            "Вход",
+			"action":                           "Влез",
+			"actionDomain":                     "Влез към адрес {domain}",
+			"all":                              "Не е твоят профил?",
+			"strategyEmailEmpty":               "Емайлът е празен.",
+			"strategyEmailInvalid":             "Емайлът е невалиден.",
+			"strategyDomainInvalid":            "Адресът {domain} не е настроен.",
+			"signinText":                       "Влез",
+			"signupText":                       "Регистрирай се",
+			"forgotText":                       "Пак са напи и не си помниш паролата?",
+			"cancelAction":                     "Отмени",
+			"footerText":                       "",
+			"emailPlaceholder":                 "Емайл",
+			"usernamePlaceholder":              "Форплеец",
+			"passwordPlaceholder":              "Парола",
+			"separatorText":                    "или",
+			"serverErrorText":                  "Имаше проблем при пенетрирането на входа.",
+			"returnUserLabel":                  "Последния път влиза като...",
+			"domainUserLabel":                  "Вие сте свързани от корпоративнат ви мрежа...",
+			"wrongEmailPasswordErrorText":      "Навлиден емайл или парола.",
+			"passwordChangeRequiredErrorText":  "Трябва да си смените паролата.",
+			"unauthorizedErrorText":            "Нямата достъп.",
+			"userBlockedErrorText":             "Профилът ви е блокриан",
+			"or":                               "... или влезта като",
+			"loadingMessage":                   "Влизане като {connection}...",
+			"popupCredentials":                 "Въведте кодовете си за достъп",
+			"userClosedPopup":                  "Прозорецът се затвори. Опитай пак.",
+			"userConsentFailed":                "Нямате позволение. Опитай пак."
 		},
 		
 		"signup": {
-		"description":                 "",
-		"title":                       "Sign Up",
-		"action":                      "Sign Up",
-		"signinText":                  "Log In",
-		"signupText":                  "Sign Up",
-		"emailPlaceholder":            "Email",
-		"usernamePlaceholder":         "Username",
-		"passwordPlaceholder":         "Create a Password",
-		"cancelAction":                "Cancel",
-		"headerText":                  "Please enter your email and password",
-		"footerText":                  "",
-		"serverErrorText":             "There was an error processing the signup.",
-		"userExistsErrorText":         "The user already exists.",
-		"signupOnSSODomainErrorText":  "This domain {domain} has been configured for Single Sign On and you can't create an account. Try signing in instead.",
-		"usernameInUseErrorText":      "The username is already in use.",
-		"invalidPassword":             "Password is too weak.",
+			"description":                 "",
+			"title":                       "Sign Up",
+			"action":                      "Sign Up",
+			"signinText":                  "Log In",
+			"signupText":                  "Sign Up",
+			"emailPlaceholder":            "Email",
+			"usernamePlaceholder":         "Username",
+			"passwordPlaceholder":         "Create a Password",
+			"cancelAction":                "Cancel",
+			"headerText":                  "Please enter your email and password",
+			"footerText":                  "",
+			"serverErrorText":             "There was an error processing the signup.",
+			"userExistsErrorText":         "The user already exists.",
+			"signupOnSSODomainErrorText":  "This domain {domain} has been configured for Single Sign On and you can't create an account. Try signing in instead.",
+			"usernameInUseErrorText":      "The username is already in use.",
+			"invalidPassword":             "Password is too weak.",				
 		
 		"passwordStrength": {
-		"nonEmpty": "Non-empty password required",
-		"lengthAtLeast": "At least %d characters in length",
-		"shouldContain": "Should contain:",
-		"containsAtLeast" : "Contain at least %d of the following %d types of characters:",
-		"lowerCase": "Lower case letters (a-z)",
-		"upperCase": "Upper case letters (A-Z)",
-		"numbers": "Numbers (i.e. 0-9)",
-		"specialCharacters" : "Special characters (e.g. !@#$%^&*)",
-		"identicalChars": "No more than %d identical characters in a row (e.g., \"%s\" not allowed)"
+			"nonEmpty": "Non-empty password required",
+			"lengthAtLeast": "At least %d characters in length",
+			"shouldContain": "Should contain:",
+			"containsAtLeast" : "Contain at least %d of the following %d types of characters:",
+			"lowerCase": "Lower case letters (a-z)",
+			"upperCase": "Upper case letters (A-Z)",
+			"numbers": "Numbers (i.e. 0-9)",
+			"specialCharacters" : "Special characters (e.g. !@#$%^&*)",
+			"identicalChars": "No more than %d identical characters in a row (e.g., \"%s\" not allowed)"
 		}
 
 		},
 		"newReset": {
-		"title":                       "Password Reset",
-		"action":                      "Send",
-		"emailPlaceholder":            "Email",
-		"cancelAction":                "Cancel",
-		"footerText":                  "",
-		"successText":                 "We've just sent you an email to reset your password.",
-		"headerText":                  "Please enter your email address. We will send you an email to reset your password.",
-		"serverErrorText":             "There was an error processing the password reset.",
-		"userDoesNotExistErrorText":   "User does not exist.",
-		"tooManyRequestsErrorText":    "You have reached the limit on password reset attempts.  Please wait before trying again."
+			"title":                       "Password Reset",
+			"action":                      "Send",
+			"emailPlaceholder":            "Email",
+			"cancelAction":                "Cancel",
+			"footerText":                  "",
+			"successText":                 "We've just sent you an email to reset your password.",
+			"headerText":                  "Please enter your email address. We will send you an email to reset your password.",
+			"serverErrorText":             "There was an error processing the password reset.",
+			"userDoesNotExistErrorText":   "User does not exist.",
+			"tooManyRequestsErrorText":    "You have reached the limit on password reset attempts.  Please wait before trying again."
 		},
 		
 		"reset": {
-		"title":                       "Password Change",
-		"action":                      "Send",
-		"emailPlaceholder":            "Email",
-		"passwordPlaceholder":         "New Password",
-		"repeatPasswordPlaceholder":   "Confirm New Password",
-		"cancelAction":                "Cancel",
-		"footerText":                  "",
-		"successText":                 "We've just sent you an email to reset your password.",
-		"enterSamePasswordText":       "Please enter the same password.",
-		"headerText":                  "Please enter your email and the new password. We will send you an email to confirm the password change.",
-		"serverErrorText":             "There was an error processing the password reset.",
-		"userDoesNotExistErrorText":   "User does not exist.",
-		"tooManyRequestsErrorText":    "You have reached the limit on password reset attempts.  Please wait before trying again.",
-		"invalidPassword":             "Password is too weak."
+			"title":                       "Password Change",
+			"action":                      "Send",
+			"emailPlaceholder":            "Email",
+			"passwordPlaceholder":         "New Password",
+			"repeatPasswordPlaceholder":   "Confirm New Password",
+			"cancelAction":                "Cancel",
+			"footerText":                  "",
+			"successText":                 "We've just sent you an email to reset your password.",
+			"enterSamePasswordText":       "Please enter the same password.",
+			"headerText":                  "Please enter your email and the new password. We will send you an email to confirm the password change.",
+			"serverErrorText":             "There was an error processing the password reset.",
+			"userDoesNotExistErrorText":   "User does not exist.",
+			"tooManyRequestsErrorText":    "You have reached the limit on password reset attempts.  Please wait before trying again.",
+			"invalidPassword":             "Password is too weak."
 		}
 	}
 	
@@ -202,14 +202,50 @@ function LoginManager() {
 		});
 	}
 	
+	this.showUserLock = function() {
+		lock.show({dict: bg, avatar: null}, function(err, profile, token) {
+			if (err) {
+
+			} else { 
+				localStorage.setItem('userToken', token);
+				
+				window.userProfile = profile;
+				
+				self.renderUserUI();
+				
+				if (window.userProfile['app_metadata']['roles'][0] != 'admin' &&
+                    window.userProfile['app_metadata']['roles'][0] != 'superadmin') {
+				
+				} else {
+					
+				}
+			} 
+		});
+	}
+	
 	this.getUserInfo = function() {
 		var token = localStorage.getItem('userToken');
 				
 		if (token) {
-			lock.getProfile(token, function(error, profile) {
-				localStorage.setItem('userToken', token);
+			lock.getProfile(token, function(err, profile) {
+				if(!err) {
+					localStorage.setItem('userToken', token);
+					
+					window.userProfile = profile;
+					
+					self.renderUserUI();
+				}
 			});
 		}
+	}
+	
+	this.renderUserUI = function() {
+		var $login = $('#userLogin'),
+			$user =$('#userLogin b');
+		
+		$login.css('background-image', 'url(' + window.userProfile.picture + ')');
+		$login.attr('aria-pressed', true);
+		$user.text(window.userProfile.name.split(' ')[0] || window.userProfile.nickname);
 	}
 	 
 	
@@ -218,6 +254,10 @@ function LoginManager() {
 	/** 
 	 * EVENTS
 	 */
+	
+	/**
+	 * Admin
+	 */ 
 	 
 	$(window).on('load', function(e) {
 		$('#login img.svg').each(function() {
@@ -243,5 +283,13 @@ function LoginManager() {
 	
 	$('#login').on('click', 'button.login', function (e) {
 		window.location.href = "foradmin.html#main";
+	});
+	
+	/**
+	 * Forplay
+	 */
+	
+	$('#userLogin').click(function(e) { 
+		self.showUserLock();
 	});
 }
